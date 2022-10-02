a23f1c11d3d43517660a48e0d04469858ab53a5b
Luke-Eom <eluke1028@gmail.com>
2022-10-02 21:37:26 +0900

    base

---
+++ FPBoot/src/main/java/kr/pe/cshop/model/controller/MemoryCardController.java
@@ -0,0 +1,10 @@
+package kr.pe.cshop.model.controller;
+
+import kr.pe.cshop.model.service.MemoryCardService;
+
+public class MemoryCardController {
+
+	private MemoryCardService mcService;
+	
+	//GET,POST,PUT,DELETE - service & dao(repository app. interface abstract...)
+}
