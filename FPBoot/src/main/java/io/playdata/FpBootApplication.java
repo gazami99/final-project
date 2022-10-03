df4a86887fd573e33632bd0e85fdccac857ad8db
gazami99 <106167682+gazami99@users.noreply.github.com>
2022-10-03 13:59:58 +0900

    Add files via upload

---
+++ FPBoot/src/main/java/io/playdata/FpBootApplication.java
@@ -0,0 +1,13 @@
+package io.playdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpBootApplication.class, args);
+	}
+
+}
