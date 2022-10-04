9c4ec072ea283a2bc83f7964f9b14a935ee2f72a
gazami99 <106167682+gazami99@users.noreply.github.com>
2022-10-04 10:38:56 +0900

    Delete FPBoot directory

--- FPBoot/src/main/java/kr/pe/cshop/model/dao/MemoryCardRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.pe.cshop.model.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import kr.pe.cshop.model.domain.MemoryCard;
-
-public interface MemoryCardRepository extends CrudRepository<MemoryCard,String> {
-
-}
