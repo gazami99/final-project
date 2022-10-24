0874bb2e7da30e3ceffe5052f3e4d319494bce56
gazami99 <106167682+gazami99@users.noreply.github.com>
2022-10-25 02:07:39 +0900

    Add files via upload

--- FPBoot-deploy/src/test/java/kr/pe/cshop/FpBootDeployApplicationTests.java
+++ FPBoot-deploy/src/test/java/kr/pe/cshop/FpBootDeployApplicationTests.java
@@ -2,7 +2,8 @@ package kr.pe.cshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.context.web.WebAppConfiguration;
+@WebAppConfiguration 
 @SpringBootTest
 class FpBootDeployApplicationTests {
 
