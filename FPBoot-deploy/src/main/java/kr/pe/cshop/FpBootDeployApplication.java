f5bc586db955a57d0ad8db2708f82b1fab6f209d
gazami99 <106167682+gazami99@users.noreply.github.com>
2022-10-25 02:02:05 +0900

    Add files via upload

---
+++ FPBoot-deploy/src/main/java/kr/pe/cshop/FpBootDeployApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.cshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpBootDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpBootDeployApplication.class, args);
+	}
+
+}
