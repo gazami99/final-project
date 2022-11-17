4ca7bca2d0d024fb7f8e6110f2b12bfb0eecf205
gazami99 <106167682+gazami99@users.noreply.github.com>
2022-11-17 16:08:40 +0900

    Update test_python_CI.py

--- Python-deploy/test_python_CI.py
+++ Python-deploy/test_python_CI.py
@@ -3,7 +3,7 @@ from package.MLSerivce import findbest
 def mlService():
   
   test_dict = {'최저가':700000,'용도':0}
-  ml_dict = findbest(test_dict)
+  ml_dict = find_best(test_dict)
   
   assert ml_dict.keys() != None
   
